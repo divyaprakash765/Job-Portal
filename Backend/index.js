677d3d2e2394c6bc1502e93a3cbfe22425cfa6e8
divya prakash <2022ugce092@nitjsr.ac.in>
2025-07-08 01:24:54 +0530

    feat: Update API endpoints to use production URL and configure CORS for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-portal-et5n.onrender.com',
     credentials:true
 }
 
